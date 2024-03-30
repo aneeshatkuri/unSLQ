8310094965c7ff8fedcdb16fe024ff65ad64f907
Aneesh Atkuri <42298920+aneeshatkuri@users.noreply.github.com>
2024-03-30 13:27:38 -0400

    Update sql.py

--- sql.py
+++ sql.py
@@ -10,7 +10,7 @@ Create table STUDENT(NAME VARCHAR(25), ROLE VARCHAR(25), COMPANY VARCHAR(25), LO
 
 cursor.execute(table_info)
 
-cursor.execute('''Insert Into STUDENT values('Aneesh', 'Product Management Intern', 'Juniper Networks', 'Sunnyvale', 55)''')
+cursor.execute('''Insert Into STUDENT values('Aneesh', 'Product Management Intern', 'Juniper Networks', 'Sunnyvale', 60)''')
 cursor.execute('''Insert Into STUDENT values('Manohar', 'Machine Learning Intern', 'Apple', 'Seattle', 52)''')
 cursor.execute('''Insert Into STUDENT values('Abhinav', 'Software Engineer Intern', 'Oracle', 'Redwood City', 50)''')
 cursor.execute('''Insert Into STUDENT values('Aadesh', 'Deep Learning Architect', 'NVIDIA', 'Santa Clara', 54)''')
