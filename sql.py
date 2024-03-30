63099e54fa25fa6355b9bf789754cf73ee4271d3
Aneesh Atkuri <42298920+aneeshatkuri@users.noreply.github.com>
2024-03-30 13:25:51 -0400

    Update sql.py

--- sql.py
+++ sql.py
@@ -5,16 +5,16 @@ connection = sqlite3.connect("student.db")
 cursor = connection.cursor()
 
 table_info = """
-Create table STUDENT(NAME VARCHAR(25), ROLE VARCHAR(25), LOCATION VARCHAR(25), SALARY INT)
+Create table STUDENT(NAME VARCHAR(25), ROLE VARCHAR(25), COMPANY VARCHAR(25), LOCATION VARCHAR(25), SALARY INT)
 """
 
 cursor.execute(table_info)
 
-cursor.execute('''Insert Into STUDENT values('Aneesh', 'Product Management Intern', 'Sunnyvale', $55)''')
-cursor.execute('''Insert Into STUDENT values('Manohar', 'Machine Learning Intern', 'Seattle', $52)''')
-cursor.execute('''Insert Into STUDENT values('Abhinav', 'Software Engineer Intern', 'Redwood City', $50)''')
-cursor.execute('''Insert Into STUDENT values('Aadesh', 'Deep Learning Architect', 'Santa Clara', $54)''')
-cursor.execute('''Insert Into STUDENT values('Hariharan', 'Data Science Intern', 'Dallas', $45)''')
+cursor.execute('''Insert Into STUDENT values('Aneesh', 'Product Management Intern', 'Juniper Networks', 'Sunnyvale', 55)''')
+cursor.execute('''Insert Into STUDENT values('Manohar', 'Machine Learning Intern', 'Apple', 'Seattle', 52)''')
+cursor.execute('''Insert Into STUDENT values('Abhinav', 'Software Engineer Intern', 'Oracle', 'Redwood City', 50)''')
+cursor.execute('''Insert Into STUDENT values('Aadesh', 'Deep Learning Architect', 'NVIDIA', 'Santa Clara', 54)''')
+cursor.execute('''Insert Into STUDENT values('Hariharan', 'Data Science Intern', 'Quorvo', 'Dallas', 45)''')
 
 print("The inserted records are")
 
