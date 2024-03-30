da9abd125e81a6abdce31c22ffea7e845ed478e5
Aneesh Atkuri <42298920+aneeshatkuri@users.noreply.github.com>
2024-03-30 13:36:11 -0400

    Update sql.py

--- sql.py
+++ sql.py
@@ -11,9 +11,9 @@ Create table STUDENT(NAME VARCHAR(25), ROLE VARCHAR(25), COMPANY VARCHAR(25), LO
 cursor.execute(table_info)
 
 cursor.execute('''Insert Into STUDENT values('Aneesh', 'Product Management Intern', 'Juniper Networks', 'Sunnyvale', 60)''')
-cursor.execute('''Insert Into STUDENT values('Manohar', 'Machine Learning Intern', 'Apple', 'Seattle', 52)''')
+cursor.execute('''Insert Into STUDENT values('Manohar', 'Machine Learning Engineer Intern', 'Apple', 'Seattle', 52)''')
 cursor.execute('''Insert Into STUDENT values('Abhinav', 'Software Engineer Intern', 'Oracle', 'Redwood City', 50)''')
-cursor.execute('''Insert Into STUDENT values('Aadesh', 'Deep Learning Architect', 'NVIDIA', 'Santa Clara', 54)''')
+cursor.execute('''Insert Into STUDENT values('Aadesh', 'Deep Learning Architect Intern', 'NVIDIA', 'Santa Clara', 54)''')
 cursor.execute('''Insert Into STUDENT values('Hariharan', 'Data Science Intern', 'Quorvo', 'Dallas', 45)''')
 
 print("The inserted records are")
